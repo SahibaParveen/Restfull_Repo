9dc342b52b69e6dfd7381ed320cec92e6c156720
user <user@192.168.0.24>
2019-08-28 23:28:26 +0530

    Added user details

--- mobileApp/src/main/java/com/restfull/mobileApp/ws/user/ui/model/response/UserDetails.java
+++ mobileApp/src/main/java/com/restfull/mobileApp/ws/user/ui/model/response/UserDetails.java
@@ -1,5 +1,26 @@
 package com.restfull.mobileApp.ws.user.ui.model.response;
 
 public class UserDetails {
-
+	
+	
+	private String name;
+	private int contact;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getContact() {
+		return contact;
+	}
+	public void setContact(int contact) {
+		this.contact = contact;
+	}
+	
+	@Override
+	public String toString() {
+		return "UserDetails [name=" + name + ", contact=" + contact + "]";
+	}	
 }
